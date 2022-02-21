420466518fa58ceb4b5dc1d3313655a61cb45851
zoniha <awaawapurin@gmail.com>
2022-02-21 20:07:24 +0900

    ✨ feat: Add new components

--- src/components/Task/TaskCard.jsx
+++ src/components/Task/TaskCard.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
+import { TaskCardTitle } from './TaskCardTitle';
+import { TaskCardDeleteButton } from './Button/TaskCardDeleteButton';
+import { TaskAddInput } from './input/TaskAddInput';
+import { Tasks } from './Tasks';
+
 export const TaskCard = () => {
-  return <div>TaskCard</div>;
+  return (
+    <div>
+      <TaskCardTitle />
+      <TaskCardDeleteButton />
+      <TaskAddInput />
+      <Tasks />
+    </div>
+  );
 };
